5c90b9be01be01969ae7c4db6ac5cf8474599d2a
Alexander Baker <alex.baker685@gmail.com>
2018-10-26 15:36:24 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,10 +4,8 @@ def position_taken?(board, index)
 if board[index] == 'X' || board[index] == 'O'
  return true
 
-  elsif board[index] != " "
+  elsif board[index] != " " || board[index] != ""
     return false
 
-  elsif board[index] != ""
-    return false
   end
 end
